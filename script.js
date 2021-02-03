eb4f0b84f5c5140615cdee4557dda5fabe3dc2e1
Brijeshjha670 <brijeshjha18011997@gmail.com>
2021-02-02 22:40:57 -0800

    created functionality of show single meal page when we click on img

--- script.js
+++ script.js
@@ -46,6 +46,67 @@ function searchMeal(e) {
     alert('Please enter a search term');
   }
 }
+//Fetch meal by id
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      //showing that meal to the dom
+      addMealToDOM(meal);
+    });
+}
+
+//Add meal to DOM
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+  <div class="single-meal">
+  <h1>${meal.strMeal}</h1>
+  <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+  <div class="single-meal-info">
+    ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+    ${meal.strArea ? `<p>${meal.strArea}</P>` : ''}
+    </div>
+    <div class="main">
+    <p>${meal.strInstruction}</P>
+    <h2>Ingredients</h2>
+    <ul>
+        ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+        </ul>
+        </div>
+        
+        
+  </div>`;
+}
 
 //Event listners
 submit.addEventListener('submit', searchMeal);
+
+mealsEl.addEventListener('click', (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  });
+
+  //for getting the meal id
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid');
+    getMealById(mealID);
+  }
+});
